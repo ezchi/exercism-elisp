0f48ad018b2c2e8568d9d881872e06738f15580b
Enze Chi <Enze.Chi@gmail.com>
2019-10-01 21:19:36 +1000

    Use Gauss formula to calculate sum of squares

--- difference-of-squares/difference-of-squares.el
+++ difference-of-squares/difference-of-squares.el
@@ -7,17 +7,11 @@
 
 (defun square-of-sum (n)
   "Return square of sum from 1 to `N'."
-  (let ((sum (/ (* (1+ n) n) 2)))
-    (* sum sum)))
+  (expt (/ (* (1+ n) n) 2) 2))
 
 (defun sum-of-squares (n)
   "Return sum of square from 1 to `N'."
-  (let ((sum 0)
-        (count 1))
-    (while (<= count n)
-      (setq sum (+ sum (* count count)))
-      (setq count (1+ count)))
-    sum))
+  (/ (* n (1+ n) (1+ (* n 2))) 6))
 
 (defun difference (n)
   "Return the difference between sum of squares and square of sum from 1 to `N'."
