f19b7b18fb2e9fe2a62e6b1f4cc9b3b2e4c8e8ed
Enze Chi <Enze.Chi@gmail.com>
2019-10-18 22:37:38 +1100

    Use replace-regexp-in-string solution

--- atbash-cipher/atbash-cipher.el
+++ atbash-cipher/atbash-cipher.el
@@ -6,26 +6,19 @@
 
 
 ;;; Code:
-(defun encode-char (c)
-  (setq index (1+ index))
-  (when (= (% index 6) 0)
-    (setq index (1+ index))
-    (setq ciphertext (format "%s " ciphertext)))
-  (setq ciphertext (format "%s%c" ciphertext c)))
+
+(require 'subr-x)
 
 (defun encode (plaintext)
   "Encode PLAINTEXT to atbash-cipher encoding."
-  (let ((plaintext (downcase plaintext))
-        (index 0)
-        (ciphertext ""))
-    (mapc (lambda (c)
-            (when (<= ?a c ?z)
-              (encode-char (- ?z (- c ?a))))
-            (when (<= ?0 c ?9)
-              (encode-char c)))
-          plaintext)
-    ciphertext))
-
+  (string-trim
+   (replace-regexp-in-string
+    ".\\{5\\}"
+    (lambda (s) (concat s " "))
+    (apply 'string
+           (mapcar
+            (lambda (c) (if (<= ?a c ?z) (- ?z (- c ?a)) c))
+            (replace-regexp-in-string "[^a-z0-9]" "" (downcase plaintext)))))))
 
 (provide 'atbash-cipher)
 ;;; atbash-cipher.el ends here
