151b1850464d6873d0f6a2a09a92aad94e1a4313
Enze Chi <Enze.Chi@gmail.com>
2019-10-30 20:39:53 +1100

    Add perfect numbers solution

---
+++ perfect-numbers/perfect-numbers.el
@@ -0,0 +1,27 @@
+;;; perfect-numbers.el --- perfect-numbers Exercise (exercism)
+
+;;; Commentary:
+
+;;; Code:
+
+(defun aliquot-sum (n)
+  "Calculates aliquot sum of N."
+  (let ((sum 1))
+    (if (= n 1)
+        0
+      (mapc (lambda (d)
+              (when (= (mod n d) 0)
+                (setq sum (+ sum d))))
+            (number-sequence 2 (/ (1+ n) 2)))
+      sum)))
+
+(defun classify (number)
+  "Do something with NUMBER here."
+  (cond
+   ((< number 0) (error "Classification is only possible for natural numbers"))
+   ((= number (aliquot-sum number)) 'perfect)
+   ((< number (aliquot-sum number)) 'abundant)
+   ((> number (aliquot-sum number)) 'deficient)))
+
+(provide 'perfect-numbers)
+;;; perfect-numbers.el ends here
