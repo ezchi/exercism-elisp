70c3f12c073e043276d45bc0fa90ed36b57a697e
Enze Chi <Enze.Chi@gmail.com>
2019-11-10 10:12:06 +1100

    Use string-join/mapcar*/mapcar/seq-partition/thread-last

--- crypto-square/crypto-square.el
+++ crypto-square/crypto-square.el
@@ -4,27 +4,20 @@
 
 ;;; Code:
 
+(require 'subr-x)
+(require 'seq)
+(require 'cl-lib)
+
 (defun encipher (s)
   (let* ((s (replace-regexp-in-string "[^0-9a-z]" "" (downcase s)))
-         (row (ceiling (sqrt (length s))))
-         (col (if (< (* (1- row) row) (length s))
-                  row
-                (1- row)))
-         (r (format "\\([0-9a-z ]\\{%0d\\}\\)" col))
-         (row-index 0)
-         (col-index 0)
-         (result (make-vector (* row col) ?\ )))
+         (rows (ceiling (sqrt (length s))))
+         (fmt (format "%%-%ds" rows)))
     (if (= 0 (length s))
         ""
-      (mapc (lambda (c)
-              (aset result (+ (* row-index col) col-index) c)
-              (setq row-index (1+ row-index))
-              (when (= row-index row)
-                (setq row-index 0)
-                (setq col-index (1+ col-index))))
-            s)
-      (substring (replace-regexp-in-string r "\\1 " (concat result)) 0 (+ (1- row) (* row col))))))
-
+      (string-join (thread-last (seq-partition s rows)
+                     (mapcar (lambda(c) (format fmt c)))
+                     (apply #'mapcar* #'string))
+                   " "))))
 
 (provide 'crypto-square)
 ;;; crypto-square.el ends here
